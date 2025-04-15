666462ca4632ee3fdc0a14a2cbc86cfb0b4da662
vicllo <61057360+vicllo@users.noreply.github.com>
2025-04-15 11:37:10 +0200

    Add the chunk structure
    On a pas mis de fonction update ou render pour le moment. On initalise simplement le chunk et on le détruit

---
+++ src/voxel/chunk.hpp
@@ -0,0 +1,12 @@
+#include "block.hpp"
+
+class Chunk {
+	public:
+	Chunk();
+	~Chunk();
+	//void Update(float dt);
+	// void Render(OpenGLRenderer* pRenderer);
+	static const int CHUNK_SIZE = 16;
+	private: // The blocks data
+		Block*** m_pBlocks;
+};
