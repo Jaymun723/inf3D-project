84434c9c9a4d7474d3bdb12468fd1131047458ac
vicllo <61057360+vicllo@users.noreply.github.com>
2025-05-04 16:37:05 +0200

    Add a rule example

---
+++ src/markov/rules_examples.cpp
@@ -0,0 +1,14 @@
+#include "rules_examples.hpp"
+
+
+Rule left_disapear = Rule(
+	{	vec3(0, 0, 0),
+		vec3(-1, 0, 0) },
+	{	BlockType_Default, 
+		BlockType_Empty, 
+	},
+	BlockType_Empty
+);
+
+
+MarkovRule keep_only_full = MarkovRule({ left_disapear });
---
+++ src/markov/rules_examples.hpp
@@ -0,0 +1,8 @@
+#include "markov/markov.hpp"
+
+
+#include "cgp/cgp.hpp"
+
+using namespace cgp;
+
+extern MarkovRule keep_only_full;
