bdb08a5e45eab7a5c38621fd505c20e1de1c5c81
vicllo <61057360+vicllo@users.noreply.github.com>
2025-05-15 09:36:49 +0200

    Update rule.hpp
    Add a probability parameter

--- src/markov/rule.hpp
+++ src/markov/rule.hpp
@@ -8,10 +8,11 @@ using namespace cgp;
 
 class Rule {
 	public:
-		Rule(std::vector<vec3> relative_positions, std::vector<BlockType> relative_types, BlockType result);
+		Rule(std::vector<vec3> relative_positions, std::vector<BlockType> relative_types, BlockType result, float probability);
 		std::vector<vec3> relative_positions;
 		std::vector<BlockType> relative_types;
 		BlockType result;
+		float probability;
 		bool applies_to(const Chunk &C, const vec3& pos) const;
 		void apply(Chunk &C, const vec3& pos) const;
 };
