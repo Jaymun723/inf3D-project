06db13d7e3432426656a6c05bb1524b55427c7f1
vicllo <61057360+vicllo@users.noreply.github.com>
2025-06-05 19:19:19 +0200

    Improve River

--- src/markov/structures/river.cpp
+++ src/markov/structures/river.cpp
@@ -48,12 +48,94 @@ void BuildRiverRule::apply(Chunk &C, const vec3 &pos) const
 
 ExtendedMR BuildRiver = ExtendedMR({std::make_shared<BuildRiverRule>()});
 
-bool RemoveGrassTmpRule::applies_to(const Chunk &C, const vec3 &pos) const
+bool MakeGrassTmpRule::applies_to(const Chunk &C, const vec3 &pos) const
+{
+	return (C.m_pBlocks[(int)pos.x][(int)pos.y][(int)pos.z].block_type == BlockType_Grass);
+}
+
+void MakeGrassTmpRule::apply(Chunk &C, const vec3 &pos) const
+{
+	int x = pos.x;
+	int y = pos.y;
+	int z = pos.z;
+	C.m_pBlocks[x][y][z].block_type = BlockType_Grass_Tmp;
+}
+
+ExtendedMR MakeGrassTmp = ExtendedMR({std::make_shared<MakeGrassTmpRule>()});
+
+
+bool ElevateGrassRule::applies_to(const Chunk& C, const vec3& pos) const {
+	if (C.m_pBlocks[(int)pos.x][(int)pos.y][(int)pos.z].block_type != BlockType_Grass_Tmp) {
+		return false;
+	}
+	if (pos.z + 1 >= C.BLOCK_CHUNK_SIZE.z) {
+		return false;
+	}
+	if (C.m_pBlocks[(int)pos.x][(int)pos.y][(int)pos.z + 1].block_type != BlockType_Empty) {
+		return false;
+	}
+	vec3 directions[24] = { 
+		vec3(1, 0, 0), vec3(-1, 0, 0), vec3(0, 1, 0), vec3(0, -1, 0), vec3(1, 1, 0), vec3(1, -1, 0), vec3(-1, -1, 0), vec3(-1, 1, 0),
+		vec3(2, 0, 0), vec3(2, 1, 0), vec3(2, 2, 0), vec3(1, 2, 0), vec3(0, 2, 0), vec3(-1, 2, 0), vec3(-2, 2, 0), vec3(-2, 1, 0), vec3(-2, 0, 0), vec3(-2, -1, 0), vec3(-2, -2, 0), vec3(-1, -2, 0), vec3(0, -2, 0), vec3(1, -2, 0), vec3(2, -2, 0), vec3(2, -1, 0)};
+	int count = 0;
+	for (const vec3& dir : directions)
+	{
+		vec3 new_pos = pos + dir;
+		if (new_pos.x < 0 || new_pos.x >= C.BLOCK_CHUNK_SIZE.x ||
+			new_pos.y < 0 || new_pos.y >= C.BLOCK_CHUNK_SIZE.y ||
+			new_pos.z < 0 || new_pos.z >= C.BLOCK_CHUNK_SIZE.z)
+		{
+			++count;
+			continue; // Out of bounds
+		}
+		if (C.m_pBlocks[(int)new_pos.x][(int)new_pos.y][(int)new_pos.z].block_type == BlockType_Grass
+			|| C.m_pBlocks[(int)new_pos.x][(int)new_pos.y][(int)new_pos.z].block_type == BlockType_Grass_Tmp) {
+			++count;
+		}
+	}
+	return count > 20;
+}
+
+void ElevateGrassRule::apply(Chunk& C, const vec3& pos) const
+{
+	int x = pos.x;
+	int y = pos.y;
+	int z = pos.z;
+	C.m_pBlocks[x][y][z].block_type = BlockType_Grass;
+	vec3 directions[24] = {
+		vec3(1, 0, 0), vec3(-1, 0, 0), vec3(0, 1, 0), vec3(0, -1, 0), vec3(1, 1, 0), vec3(1, -1, 0), vec3(-1, -1, 0), vec3(-1, 1, 0),
+		vec3(2, 0, 0), vec3(2, 1, 0), vec3(2, 2, 0), vec3(1, 2, 0), vec3(0, 2, 0), vec3(-1, 2, 0), vec3(-2, 2, 0), vec3(-2, 1, 0), vec3(-2, 0, 0), vec3(-2, -1, 0), vec3(-2, -2, 0), vec3(-1, -2, 0), vec3(0, -2, 0), vec3(1, -2, 0), vec3(2, -2, 0), vec3(2, -1, 0) };
+	int count = 0;
+	for (const vec3& dir : directions)
+	{
+		vec3 new_pos = pos + dir;
+		if (new_pos.x < 0 || new_pos.x >= C.BLOCK_CHUNK_SIZE.x ||
+			new_pos.y < 0 || new_pos.y >= C.BLOCK_CHUNK_SIZE.y ||
+			new_pos.z < 0 || new_pos.z >= C.BLOCK_CHUNK_SIZE.z)
+		{
+			++count;
+			continue; // Out of bounds
+		}
+		if (C.m_pBlocks[(int)new_pos.x][(int)new_pos.y][(int)new_pos.z].block_type == BlockType_Grass
+			|| C.m_pBlocks[(int)new_pos.x][(int)new_pos.y][(int)new_pos.z].block_type == BlockType_Grass_Tmp) {
+			++count;
+		}
+	}
+	if (20 + rand() % 4 < count) {
+		C.m_pBlocks[x][y][z+1].block_type = BlockType_Grass_Tmp;
+	}
+}
+
+ExtendedMR ElevateGrass = ExtendedMR({ std::make_shared<ElevateGrassRule>() });
+
+
+
+bool RemoveGrassTmpRule::applies_to(const Chunk& C, const vec3& pos) const
 {
 	return (C.m_pBlocks[(int)pos.x][(int)pos.y][(int)pos.z].block_type == BlockType_Grass_Tmp);
 }
 
-void RemoveGrassTmpRule::apply(Chunk &C, const vec3 &pos) const
+void RemoveGrassTmpRule::apply(Chunk& C, const vec3& pos) const
 {
 	int x = pos.x;
 	int y = pos.y;
@@ -61,7 +143,8 @@ void RemoveGrassTmpRule::apply(Chunk &C, const vec3 &pos) const
 	C.m_pBlocks[x][y][z].block_type = BlockType_Grass;
 }
 
-ExtendedMR RemoveGrassTmp = ExtendedMR({std::make_shared<RemoveGrassTmpRule>()});
+ExtendedMR RemoveGrassTmp = ExtendedMR({ std::make_shared<RemoveGrassTmpRule>() });
+
 
 int build_river_aux(Chunk &C, int step)
 {
@@ -95,11 +178,26 @@ int build_river_aux(Chunk &C, int step)
 		break;
 
 	case 3:
-		if (RemoveGrassTmp.applyRule(C, 1))
+		if (MakeGrassTmp.applyRule(C, 5))
 		{
 			break;
 		}
 		step = 4;
+		break;
+
+	case 4:
+		if (ElevateGrass.applyRule(C, -1))
+		{
+			break;
+		}
+		step = 5;
+		break;
+
+	case 5:
+		if (RemoveGrassTmp.applyRule(C, 5)) {
+			break;
+		}
+		step = 6;
 		break;
 
 	default:
--- src/markov/structures/river.hpp
+++ src/markov/structures/river.hpp
@@ -13,6 +13,20 @@ public:
 };
 
 
+class MakeGrassTmpRule : public Rule {
+public:
+	bool applies_to(const Chunk& C, const vec3& pos) const override;
+	void apply(Chunk& C, const vec3& pos) const override;
+};
+
+
+class ElevateGrassRule : public Rule {
+public:
+	bool applies_to(const Chunk& C, const vec3& pos) const override;
+	void apply(Chunk& C, const vec3& pos) const override;
+};
+
+
 class RemoveGrassTmpRule : public Rule {
 public:
 	bool applies_to(const Chunk& C, const vec3& pos) const override;
